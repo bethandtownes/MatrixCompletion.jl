931d48bebf0204c79a06efe5c3f7bc2d90269e1f
jasonsun0310 <ds653@cornell.edu>
2019-07-11 15:06:05 -0400

    Files generated by PkgTemplates

---
+++ src/MatrixCompletion.jl
@@ -0,0 +1,5 @@
+module MatrixCompletion
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MatrixCompletion
+using Test
+
+@testset "MatrixCompletion.jl" begin
+    # Write your own tests here.
+end
