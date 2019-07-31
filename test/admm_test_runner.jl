0b58cb5084023862d186d5bc689ed2c67bb72dc2
jasonsun0310 <ds653@cornell.edu>
2019-07-31 18:48:06 -0400

    update

--- test/admm_test_runner.jl
+++ test/admm_test_runner.jl
@@ -10,25 +10,75 @@ include("./admm_test.jl")
 test_admm_with_autodiff_smallinput(gd_iter=3,dbg=true)
 test_admm_without_autodiff_smallinput(gd_iter=3)
 test_admm_without_autodiff_largeinput(gd_iter=3,dbg=true)
-
-a = Random.rand(6000,6000).-0.5
-b = Random.rand(6000,6000)
-c = Random.rand(6000,6000)
-
-
-@time a * b * c
-@time (a * b) * c
+#
+# a = Random.rand(3000,3000).-0.5
+# b = Random.rand(6000,6000)
+# c = Random.rand(6000,6000)
+#
+#
+# @time a * b * c
+# @time (a * b) * c
 
 eigen(a)
-
-
-
+a = Random.rand(10000,10000)
 asym = a' + a
 
-m = Symmetric(asym)
+t  = asym * asym
+# m = Symmetric(asym)
+#
+# @time eigen(m)
+#
+# @time eigen(m,0,100)
+#
+# @time eigvals(m)
+# @time eigvecs(m)
+# import LowRankApprox
+#
+#
+# a = Random.rand(2000,2000)
+# m = Symmetric(a+a')
+# @time LowRankApprox.pheigfact(m)
+#
+# @time eigen(m)
+#
 
-@time eigen(m)
+
+
+a = Random.rand(5000,5000)
+
+ta = a'+a
+@time eigs(ta, nev = 20, which=:BE)
+
+@time eigen(ta)
 
 
 
-@time eigen(m,0,100)
+
+
+
+
+
+
+using Arpack
+using LinearAlgebra
+import Random
+
+
+# eig_dec = eigen(test_matrix);
+F = svd(test_matrix)
+truncated = deepcopy(F.S)
+truncated[51:end] .= 0
+truncated_mat = F.U * Diagonal(truncated) * F.Vt
+rank(truncated_mat)
+a_truncated = truncated_mat[101:200,1:100]
+rank(a_truncated)
+
+a = Random.rand(2500,2500)
+b = Random.rand(2500,2500)
+c = Random.rand(2500,2500)
+a = a+a'
+b = b+b'
+c = c+c'
+test_matrix = [b a;a' c]
+@time F = svd(test_matrix)
+@time F_cheap = svds(test_matrix,nsv=100)
