d0f91e988ef0047d0b4a66e11c0017d42378c376
Daqian Sun <daqiansun@macbook-pro.home>
2020-06-12 01:38:28 -0400

    update ignore

--- test/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((julia-mode
-  (package-testrunner-path . "~/.julia/dev/MatrixCompletion/test/runtests.jl")))
